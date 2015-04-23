fa1c5da18dfddb01f4b0bafcd6dffa321124270d
casperhagenaars <casperhagenaars@gmail.com>
2015-04-23 12:21:24 +0000

    test

---
+++ Opdracht1/ManyGreetings.py
@@ -0,0 +1,6 @@
+text = input("What is your name? ")
+greetings = input("How many greetings? ")
+if type(greetings) == type(1):
+   print(greetings + ' x Hello, ' + text+'!')
+else:
+    print('hoi')
